2414088433d2eb79d9debfa28655d4a7d80204e9
Vlad <askadias@mail.ru>
2014-12-01 19:15:59 +0300

    some minor changes

--- service/src/main/groovy/fraud/exceptions/FraudServiceEvent.groovy
+++ service/src/main/groovy/fraud/exceptions/FraudServiceEvent.groovy
@@ -1,7 +1,6 @@
 package fraud.exceptions
 
 import common.exceptions.EventLogBase
-import common.exceptions.EventType
 
 import static common.exceptions.EventLogBase.Level
 
@@ -10,40 +9,39 @@ enum FraudServiceEvent implements EventLogBase {
     // -------------------------------------------------------------------
     // Business events
     // -------------------------------------------------------------------
-    IDShouldNotBeNull(0, 400, EventType.InvalidInput,
+    IDShouldNotBeNull(0, 400, EventLogBase.EventType.InvalidInput,
             'Transaction id should not be null.'),
 
-    TransactionNotFound(1, 400, EventType.InvalidInput,
+    TransactionNotFound(1, 400, EventLogBase.EventType.InvalidInput,
             'Transaction with id %1$l is not found.'),
 
-    InvalidPageNumber(2, 400, EventType.InvalidInput,
+    InvalidPageNumber(2, 400, EventLogBase.EventType.InvalidInput,
             'Invalid page number provided: \'%1$s\''),
 
-    IsInBlackListAlready(3, 400, EventType.InvalidInput,
+    IsInBlackListAlready(3, 400, EventLogBase.EventType.InvalidInput,
             '%1$s with value= \'%2$s\' is in black list already'),
 
-    BlackListItemIsNotExist(4, 400, EventType.InvalidInput,
+    BlackListItemIsNotExist(4, 400, EventLogBase.EventType.InvalidInput,
             '%1$s with value= \'%2$s\' is not exist'),
 
-    VelocityConfigNotFound(5, 404, EventType.InvalidInput,
+    VelocityConfigNotFound(5, 404, EventLogBase.EventType.InvalidInput,
             'VelocityConfig for metric \'%1$s\' is not found.'),
 
-    VelocityConfigAlreadyExists(6, 400, EventType.InvalidInput,
+    VelocityConfigAlreadyExists(6, 400, EventLogBase.EventType.InvalidInput,
             'VelocityConfig for metric \'%1$s\' already exists.'),
 
-    UnexpectedErrorDuringVelocityComputation(7, 500, EventType.InternalError,
+    UnexpectedErrorDuringVelocityComputation(7, 500, EventLogBase.EventType.InternalError,
             'Unexpected error during velocity metric \'%1$s\' computation.');
 
 
-    private static final int BASE_EVENT_LOG_ID = 20000;
+    private static int BASE_EVENT_LOG_ID = 20000;
 
-    private FraudServiceEvent(final int eventId, final int responseId, final EventType eventType,
-                              final String formatString) {
+    private FraudServiceEvent(int eventId, int responseId, EventLogBase.EventType eventType, String formatString) {
         this(eventId, responseId, Level.ERROR, eventType, formatString);
     }
 
-    private FraudServiceEvent(final int eventID, final int responseId, final Level logLevel, final EventType eventType,
-                              final String formatString) {
+    private FraudServiceEvent(int eventID, int responseId, Level logLevel, EventLogBase.EventType eventType,
+                              String formatString) {
         this.eventID = BASE_EVENT_LOG_ID + eventID;
         this.responseID = responseId;
         this.logLevel = logLevel;
